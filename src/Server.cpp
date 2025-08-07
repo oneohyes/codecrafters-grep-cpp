0e02ff3712512e5a32c1dcb7c5a550c9b4b019eb
HuangCheng <oh_yes_yes@outlook.com>
2025-08-07 16:00:28 +0800

    match a literal character

--- src/Server.cpp
+++ src/Server.cpp
@@ -16,7 +16,7 @@ int main(int argc, char* argv[]) {
     std::cerr << std::unitbuf;
 
     // You can use print statements as follows for debugging, they'll be visible when running tests.
-    std::cerr << "Logs from your program will appear here" << std::endl;
+    // std::cerr << "Logs from your program will appear here" << std::endl;
 
     if (argc != 3) {
         std::cerr << "Expected two arguments" << std::endl;
@@ -33,17 +33,17 @@ int main(int argc, char* argv[]) {
 
     // Uncomment this block to pass the first stage
     //
-    // std::string input_line;
-    // std::getline(std::cin, input_line);
-    //
-    // try {
-    //     if (match_pattern(input_line, pattern)) {
-    //         return 0;
-    //     } else {
-    //         return 1;
-    //     }
-    // } catch (const std::runtime_error& e) {
-    //     std::cerr << e.what() << std::endl;
-    //     return 1;
-    // }
+    std::string input_line;
+    std::getline(std::cin, input_line);
+    
+    try {
+        if (match_pattern(input_line, pattern)) {
+            return 0;
+        } else {
+            return 1;
+        }
+    } catch (const std::runtime_error& e) {
+        std::cerr << e.what() << std::endl;
+        return 1;
+    }
 }
